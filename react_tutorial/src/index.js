bb229f92dbb4e4e4bec7986091314bf7a7b110d8
Nicole <nicoleadams.boston@gmail.com>
2022-09-18 17:39:55 -0700

    replace this props value with this state value in the square render funtion

--- react_tutorial/src/index.js
+++ react_tutorial/src/index.js
@@ -13,7 +13,7 @@ class Square extends React.Component {
     render() {
         return (
             <button className="square" onClick={function () { console.log('click'); }}>
-                {this.props.value}
+                {this.state.value}
             </button>
         );
     }
